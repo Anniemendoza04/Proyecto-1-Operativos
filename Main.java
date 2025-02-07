3f0c3ece1f7513f27509278cc5703c5c3df4f2d5
Anniemendoza04 <131669641+Anniemendoza04@users.noreply.github.com>
2025-02-07 18:41:09 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author annie
+ */
+public class Main {
+    public static void main(String[] args) {
+        SimuladorOS simulador = new SimuladorOS(2, 1000, Planificador.AlgoritmoPlanificacion.SRT);
+        simulador.pruebaSRT();
+    }
+}
